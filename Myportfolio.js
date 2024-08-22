df9f49be56c8284969ffcdd9c4db765e479b1b2c
Sai <164913655+osc672@users.noreply.github.com>
2024-08-22 16:56:23 +0530

    Update Myportfolio.js

--- Myportfolio.js
+++ Myportfolio.js
@@ -33,7 +33,7 @@ $('.menu-btn').click(function(){
 });
 // typing text animation script
 var typed = new Typed(".typing", {
-    strings: ["Student", "Fresher", "Hardworker",],
+    strings: ["Fresher", "Hardworker",],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
